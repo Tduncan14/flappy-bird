311306ffe0a3e32d9d9d57be1ae7a4ca711fde0d
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-09-03 02:33:12 -0500

    this is info for flappy bird

--- logic.js
+++ logic.js
@@ -2,4 +2,8 @@
 function draw(){
     ctx.drawImage(bg,0.0);
     ctx.drawImage(pipeNorth,pX,pY);
+    ctx.drawImage(pipeSouth,pX,pY +Const);
+    // drawing the ground
+    ctx.drawImage(fg,0,cvs.height -fg.height);
+
 }
