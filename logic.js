42a1c77459a241d66efa71acc05fac0774e30cd4
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-09-16 00:14:20 -0500

    adding images

--- logic.js
+++ logic.js
@@ -3,14 +3,14 @@ var ctx = cvs.getContext("2d");
 
 // load images by storing an image into the variable
 // giving it constuctors
-var pipeSouth = new Image();
+
 // giving them sources.
 // the routes to the folder
-
 var bird = new Image();
 var bg = new Image();
 var fg = new Image();
 var pipeNorth = new Image();
+var pipeSouth = new Image();
 
 // load images by storing an image into the variable
 // giving it constuctors
@@ -21,7 +21,7 @@ pipeNorth.src="images/pipeNorth.png";
 pipeSouth.src="images/pipeSouth.png";
 
 var gap = 85;
-var constant ;
+var constant;
 var bX = 10;
 var bY = 150;
 
@@ -35,8 +35,8 @@ var fly = new Audio();
 var scor = new Audio ();
 
 // adding the sound to the variables // updating
-fly.src ="fly.mp3";
-scor.src = "score.mp3";
+//fly.src ="fly.mp3";
+//scor.src = "score.mp3";
 
 // on key press down
 
@@ -45,7 +45,7 @@ document.addEventListener("keydown" ,moveUp);
 function moveUp(){
     bY -= 25;
     // then the sound plays
-    fly.play();
+  //  fly.play();
 }
 
 // storing the pipes in a array  because we need a lot of differnt pipes.
@@ -55,27 +55,30 @@ pipe[0] = {
     x : cvs.width,
     y : 0
 };
-
+console.log("does it work");
 
 // logic of the game
-alert("Keep moving forward");
+//alert("Keep moving forward");
 
 
 // DRAWING THE Image
 function draw(){
 
     ctx.drawImage(bg,0,0);
+
     for(var i =0; i<pipe.length;i++){
    
     // draws the pipe positon
-    constant = pipeNorth.height + gap;
+    constant = pipeNorth.height+gap;
     ctx.drawImage(pipeNorth,pipe[i].x,pipe[i].y);
     ctx.drawImage(pipeSouth,pipe[i].x,pipe[i].y+constant);
+
     pipe[i].x--;
+    
     if(pipe[i].x == 125){
         // creates a new pipe at that position, if the already existing pipes reach a certain position
         pipe.push({
-            x:cvs.width,
+            x: cvs.width,
             y:Math.floor(Math.random()*pipeNorth.height)-pipeNorth.height
         });
     }
@@ -87,7 +90,7 @@ function draw(){
     
     if(pipe[i].x == 5){
         score++;
-        scor.play();
+       // scor.play();
     }
 
     // drawing the ground
@@ -100,6 +103,8 @@ function draw(){
 
     ctx.fillStyle = "#000";
     ctx.font = "20px Verdana";
+    ctx.fillText("Score :" + score ,10, cvs.height-20);
+
     requestAnimationFrame(draw);
 }
 }
