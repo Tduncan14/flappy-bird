5fb0feb0fcbffba1454d2b64a7369d136c022fbe
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-09-15 23:24:38 -0500

    still trying to complete and finsih this game

--- logic.js
+++ logic.js
@@ -63,19 +63,33 @@ alert("Keep moving forward");
 
 // DRAWING THE Image
 function draw(){
-    for(var i =0; i<pipe.length;i++){
+
     ctx.drawImage(bg,0,0);
+    for(var i =0; i<pipe.length;i++){
+   
     // draws the pipe positon
+    constant = pipeNorth.height + gap;
     ctx.drawImage(pipeNorth,pipe[i].x,pipe[i].y);
     ctx.drawImage(pipeSouth,pipe[i].x,pipe[i].y+constant);
     pipe[i].x--;
-    if(pipe[i].x == cvs.width-188){
+    if(pipe[i].x == 125){
         // creates a new pipe at that position, if the already existing pipes reach a certain position
         pipe.push({
             x:cvs.width,
             y:Math.floor(Math.random()*pipeNorth.height)-pipeNorth.height
         });
     }
+    // detects the collision
+          
+    if( bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width && (bY <= pipe[i].y + pipeNorth.height || bY+bird.height >= pipe[i].y+constant) || bY + bird.height >=  cvs.height - fg.height){
+        location.reload(); // reload the page
+    }
+    
+    if(pipe[i].x == 5){
+        score++;
+        scor.play();
+    }
+
     // drawing the ground
     ctx.drawImage(fg,0,cvs.height -fg.height);
     ctx.drawImage(bird,bX,bY);
@@ -83,6 +97,9 @@ function draw(){
     // on a keyDown event, the bird jump
     bY += gravity;
     // keeps drawing and running the function again and agian
+
+    ctx.fillStyle = "#000";
+    ctx.font = "20px Verdana";
     requestAnimationFrame(draw);
 }
 }
